92192f29a5b876bd7f2177fc379cda8015422aa1
Mahesh <mahesh@mahesh.ws>
2014-07-09 22:47:24 +0530

    Create Auto, Taxi & Metro, Fragments & Layouts
    Create only

---
+++ mobile/src/main/java/ws/mahesh/travelmumbai/TaxiFragment.java
@@ -0,0 +1,33 @@
+package ws.mahesh.travelmumbai;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mahesh on 7/9/2014.
+ */
+public class TaxiFragment extends Fragment{
+    public TaxiFragment() {
+        super();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.auto_taxi_fare_fragment, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
