cd125c8e33cfaf94e4d88ca62d7a53cabe1647b6
Mahesh <mahesh@mahesh.ws>
2014-07-13 21:08:24 +0530

    Taxi Fragment 1/2

--- mobile/src/main/java/ws/mahesh/travelmumbai/TaxiFragment.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/TaxiFragment.java
@@ -1,16 +1,30 @@
 package ws.mahesh.travelmumbai;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ws.mahesh.travelmumbai.taxi.TaxiFareBase;
 
 /**
  * Created by Mahesh on 7/9/2014.
  */
 public class TaxiFragment extends Fragment{
+    TextView distance, reading, day_fare, night_fare, info_txt;
+    Button calcReading, calcDistance, show_chart;
+    EditText meter_reading, distance_km;
+
+    TaxiFareBase taxi=new TaxiFareBase();
+    
     public TaxiFragment() {
         super();
     }
@@ -29,5 +43,63 @@ public class TaxiFragment extends Fragment{
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        ActionBar ab=getActivity().getActionBar();
+        ab.setTitle("Travel Mumbai - Taxi");
+        distance= (TextView) getActivity().findViewById(R.id.textViewDistance);
+        reading= (TextView) getActivity().findViewById(R.id.textViewReading);
+        day_fare= (TextView) getActivity().findViewById(R.id.textViewDayFare);
+        night_fare= (TextView) getActivity().findViewById(R.id.textViewNightFare);
+
+        meter_reading= (EditText) getActivity().findViewById(R.id.editTextMeter);
+        distance_km = (EditText) getActivity().findViewById(R.id.editTextDistance);
+
+        calcDistance= (Button) getActivity().findViewById(R.id.buttoncalcDistance);
+        calcReading= (Button) getActivity().findViewById(R.id.buttoncalcReading);
+
+        calcDistance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(distance_km.getText() !=null)
+                    try {
+                        FareonDistance();
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(getActivity(), "Invalid Input", Toast.LENGTH_LONG).show();
+                        Log.e("TAXI", "Invalid input");
+                    }
+            }
+        });
+
+        calcReading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (meter_reading.getText() != null)
+                    try {
+                        FareonReading();
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Invalid Input", Toast.LENGTH_LONG).show();
+                        Log.e("TAXI", "Invalid input");
+                    }
+
+            }
+        });
+    }
+
+    private void FareonDistance() {
+        double distanced= Double.parseDouble(distance_km.getText().toString());
+        taxi.distanceBased(distanced);
+        distance.setText(""+taxi.getDistance());
+        reading.setText(""+taxi.getReading());
+        day_fare.setText(""+taxi.getFare());
+        night_fare.setText(""+taxi.getNightFare());
+    }
+
+    private void FareonReading() {
+        double meterd= Double.parseDouble(meter_reading.getText().toString());
+        taxi.readingBased(meterd);
+        distance.setText(""+taxi.getDistance());
+        reading.setText(""+taxi.getReading());
+        day_fare.setText(""+taxi.getFare());
+        night_fare.setText("" + taxi.getNightFare());
     }
 }
