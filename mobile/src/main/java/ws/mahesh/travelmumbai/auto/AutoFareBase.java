cc5ed0e43ff72fcaa0a5906b986561d454f3b8bb
Mahesh <mahesh@mahesh.ws>
2014-07-13 18:49:19 +0530

    Added Auto, Taxi Fare Calculator

--- mobile/src/main/java/ws/mahesh/travelmumbai/auto/AutoFareBase.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/auto/AutoFareBase.java
@@ -1,7 +1,60 @@
 package ws.mahesh.travelmumbai.auto;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by Mahesh on 7/9/2014.
  */
 public class AutoFareBase {
+    public double dist,fare,meter,night_fare;
+    DecimalFormat df2 = new DecimalFormat("00.00");
+
+    public void distanceBased(double tdist) {
+        dist=calculateFdistance(tdist);
+        calculateReading();
+        calculateFare();
+    }
+    public void readingBased(double tread) {
+        double tempread=tread-1.0;
+        tempread=(tempread*2)+AutoBase.MIN_DISTANCE;
+        distanceBased(tempread);
+    }
+
+    public void calculateReading() {
+        double tempdist=dist-AutoBase.MIN_DISTANCE;
+        tempdist=tempdist/2;
+        tempdist=tempdist+1;
+        meter=Double.valueOf(df2.format(tempdist));
+    }
+
+    public void calculateFare() {
+        fare=dist*AutoBase.PER_KM;
+        int tempfare=(int) Math.round(fare);
+        night_fare=tempfare+(tempfare*AutoBase.NIGHT_RATE_FACTOR);
+    }
+
+    public double calculateFdistance(double tdist) {
+        double ddist=tdist*10;
+        int tempdist=(int) ddist;
+        tempdist=(tempdist+1)/2;
+        tempdist=tempdist*2;
+        double mydist=(double) tempdist/10;
+        if(tdist>mydist)
+            mydist=mydist+0.2;
+        mydist=Double.valueOf(df2.format(mydist));
+        return mydist;
+    }
+
+    public double getDistance() {
+        return dist;
+    }
+    public int getFare() {
+        return (int)Math.round(fare);
+    }
+    public int getNightFare() {
+        return (int)Math.round(night_fare);
+    }
+    public double getReading() {
+        return meter;
+    }
 }
--- mobile/src/main/java/ws/mahesh/travelmumbai/taxi/TaxiFareBase.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/taxi/TaxiFareBase.java
@@ -1,7 +1,49 @@
 package ws.mahesh.travelmumbai.taxi;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by Mahesh on 7/9/2014.
  */
 public class TaxiFareBase {
+    public double dist,fare,meter,night_fare;
+    DecimalFormat df2 = new DecimalFormat("00.00");
+
+    public void distanceBased(double tdist) {
+        dist=Double.valueOf(df2.format(tdist));
+        calculateReading();
+        calculateFare();
+    }
+    public void readingBased(double tread) {
+        double tempread=tread-1.0;
+        tempread=(tempread*1.67)+TaxiBase.MIN_DISTANCE;
+        distanceBased(tempread);
+    }
+
+    public void calculateReading() {
+        double tempdist=dist-TaxiBase.MIN_DISTANCE;
+        tempdist=tempdist/1.67;
+        tempdist=tempdist+1;
+        meter=Double.valueOf(df2.format(tempdist));
+    }
+
+    public void calculateFare() {
+        fare=dist*TaxiBase.PER_KM;
+        int tempfare=(int) Math.round(fare);
+        night_fare=fare+(fare*TaxiBase.NIGHT_RATE_FACTOR);
+    }
+
+
+    public double getDistance() {
+        return dist;
+    }
+    public int getFare() {
+        return (int)Math.round(fare);
+    }
+    public int getNightFare() {
+        return (int)Math.round(night_fare);
+    }
+    public double getReading() {
+        return meter;
+    }
 }
