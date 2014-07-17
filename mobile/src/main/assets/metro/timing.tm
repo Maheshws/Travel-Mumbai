30355e59306b249a1f550fce0f3af2d4cc64f48f
Mahesh <mahesh@mahesh.ws>
2014-07-17 20:32:49 +0530

    Added timing info for Metro

---
+++ mobile/src/main/assets/metro/timing.tm
@@ -0,0 +1,15 @@
+<h4>Train Timings : </h4>
+<b>Weekdays (Monday to Saturday)</b>
+<ul>
+    <li>The service will commence from 05:30 hrs until 24:00 hrs.  The headway between each train will be 4 minutes at peak hours and 8 minutes at non-peak hours.</li>
+    <li>The 1st train will leave at 05:30 hrs from both terminals i.e. Versova and Ghatkopar.</li>
+    <li>The last train from Versova will leave at 23:10 hrs.</li>
+    <li>The last train from Ghatkopar will leave at 23:35 hrs.</li>
+</ul>
+<b>Weekend (Sunday)</b>
+<ul>
+    <li>The service will commence from 05:30 hrs until 24:00 hrs.  The headway between each train will be 8 minutes.</li>
+    <li>The 1st train will leave at 05:30 hrs from both terminals i.e. Versova and Ghatkopar.</li>
+    <li>The last train from Versova will leave at 23:10 hrs.</li>
+    <li>The last train from Ghatkopar will leave at 23:35 hrs.</li>
+</ul>
--- mobile/src/main/java/ws/mahesh/travelmumbai/MetroFragment.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/MetroFragment.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 import ws.mahesh.travelmumbai.metro.MetroFareBase;
+import ws.mahesh.travelmumbai.utils.MyTagHandler;
 
 /**
  * Created by Mahesh on 7/9/2014.
  */
 public class MetroFragment extends Fragment {
-    TextView tokenfare, cardfare;
+    TextView tokenfare, cardfare, timing;
     Spinner source, destination;
     Button calcFare;
     MetroFareBase metro;
@@ -45,9 +51,12 @@ public class MetroFragment extends Fragment {
         ab.setTitle("Travel Mumbai - Metro");
         tokenfare = (TextView) getActivity().findViewById(R.id.textViewTokenFare);
         cardfare = (TextView) getActivity().findViewById(R.id.textViewCardFare);
+        timing=(TextView) getActivity().findViewById(R.id.textViewTiming);
         source = (Spinner) getActivity().findViewById(R.id.spinnerSource);
         destination = (Spinner) getActivity().findViewById(R.id.spinnerDestination);
         calcFare = (Button) getActivity().findViewById(R.id.buttonCalcFare);
+
+        timing.setText(Html.fromHtml(readFromFile("metro/timing.tm"),null, new MyTagHandler()));
 
         calcFare.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +73,6 @@ public class MetroFragment extends Fragment {
         src -= 1;
         int dest = (int) destination.getSelectedItemId();
         dest -= 1;
-        //Log.e("METRO","Src="+src+"  Dest="+dest);
         if (src < 0 || dest < 0 || src == dest) {
             Toast.makeText(getActivity(), getResources().getString(R.string.invalid_option_selected), Toast.LENGTH_LONG).show();
         } else {
@@ -72,5 +80,22 @@ public class MetroFragment extends Fragment {
             tokenfare.setText(getResources().getString(R.string.fare_for_token_user) + metro.getTokenFare(src, dest));
         }
 
+    }
+
+    private String readFromFile(String fname) {
+        String str="";
+        try {
+            InputStream in= getActivity().getAssets().open(fname);
+            int size = in.available();
+            byte[] buffer = new byte[size];
+            in.read(buffer);
+            in.close();
+            str = new String(buffer);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
