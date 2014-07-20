a22464e68880f82d1894241dd243b9d73fa1f559
Mahesh <mahesh@mahesh.ws>
2014-07-20 14:28:55 +0530

    Updated Monorail UI

---
+++ mobile/src/main/java/ws/mahesh/travelmumbai/monorail/MonoListAdapter.java
@@ -0,0 +1,56 @@
+package ws.mahesh.travelmumbai.monorail;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ws.mahesh.travelmumbai.R;
+
+/**
+ * Created by Mahesh on 7/20/2014.
+ */
+public class MonoListAdapter extends ArrayAdapter<MonoListItem> {
+    private Context context;
+    private List<MonoListItem> values;
+
+    public MonoListAdapter(Context context, int resource, List<MonoListItem> values) {
+        super(context, resource, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.metro_mono_list_item, parent, false);
+        TextView dest = (TextView) rowView.findViewById(R.id.textViewDest);
+        TextView token = (TextView) rowView.findViewById(R.id.textViewTokenFare);
+        TextView card = (TextView) rowView.findViewById(R.id.textViewCardFare);
+
+        MonoListItem mono = values.get(position);
+
+        if (position == 0) {
+            dest.setTypeface(null, Typeface.BOLD);
+            token.setTypeface(null, Typeface.BOLD);
+            card.setTypeface(null, Typeface.BOLD);
+        }
+
+        dest.setText(mono.getDESTINATION());
+        token.setText(mono.getTOKEN());
+        card.setText(mono.getCARD());
+        if (mono.getCARD().equals("Rs.0")) {
+            token.setText("");
+            card.setText("");
+            dest.setTypeface(null, Typeface.BOLD_ITALIC);
+            dest.setGravity(Gravity.CENTER);
+        }
+        return rowView;
+    }
+}
