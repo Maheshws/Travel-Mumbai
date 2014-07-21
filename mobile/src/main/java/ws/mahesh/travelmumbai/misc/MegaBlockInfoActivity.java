464d6eb03c7d3512764d85932105f73cf4a3b56f
Mahesh <mahesh@mahesh.ws>
2014-07-21 21:33:15 +0530

    Added Mega Block Activity to support Parse

---
+++ mobile/src/main/java/ws/mahesh/travelmumbai/misc/MegaBlockInfoActivity.java
@@ -0,0 +1,63 @@
+package ws.mahesh.travelmumbai.misc;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import ws.mahesh.travelmumbai.R;
+
+public class MegaBlockInfoActivity extends Activity {
+    private WebView webview;
+    private static final String TAG = "Main";
+    private ProgressDialog progressBar;
+    private final String Location="http://mobondhrd.appspot.com/megablockinfo.jsp?&isusingapi=false";
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_webview);
+        this.webview = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        progressBar = ProgressDialog.show(this, "Please Wait", "Loading...");
+        webview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, "Error: " + description);
+                Toast.makeText(MegaBlockInfoActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage(description);
+                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+        webview.loadUrl(Location);
+    }
+}
