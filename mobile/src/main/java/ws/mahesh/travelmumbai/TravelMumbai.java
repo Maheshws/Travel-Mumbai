4a787f2a1cd0a29a1d588c391286f9beefc9282f
Mahesh <mahesh@mahesh.ws>
2014-07-21 21:32:48 +0530

    Added *Parse*

---
+++ mobile/src/main/java/ws/mahesh/travelmumbai/TravelMumbai.java
@@ -0,0 +1,84 @@
+package ws.mahesh.travelmumbai;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.format.Formatter;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import ws.mahesh.travelmumbai.misc.MegaBlockInfoActivity;
+
+import static android.provider.Settings.Secure;
+
+/**
+ * Created by Mahesh on 7/21/2014.
+ */
+public class TravelMumbai extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this,getString(R.string.parseAppID),getString(R.string.parseClientKey));
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+        String  android_id = Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
+
+        SharedPreferences pref = getSharedPreferences("user_settings", 0); // 0 - for private mode
+        Boolean Subscriber=pref.getBoolean("MegaBlock",false);
+        if(!Subscriber)
+            PushService.setDefaultPushCallback(this, MegaBlockInfoActivity.class);
+        else
+            PushService.setDefaultPushCallback(this,null);
+
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("UniqueId",android_id);
+        installation.put("IPAddress",getLocalIpAddress());
+        installation.put("MACAddress",getMACAddress());
+        installation.saveEventually();
+
+    }
+
+    private String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        String ip = Formatter.formatIpAddress(inetAddress.hashCode());
+                        return ip;
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("log_tag", "Error Obtaining IP Address " + ex.toString());
+        }
+        return "NA";
+    }
+    private String getMACAddress() {
+        String macadd;
+        try {
+            //Get MAC Address
+            WifiManager manager = (WifiManager) getSystemService(WIFI_SERVICE);
+            WifiInfo info = manager.getConnectionInfo();
+            macadd = info.getMacAddress();
+        } catch (Exception e1) {
+            macadd = "NA";
+            Log.e("log_tag", "Error Optaining MAC ID " + e1.toString());
+        }
+        if(macadd==null)
+            macadd="NA";
+        return macadd;
+    }
+}
