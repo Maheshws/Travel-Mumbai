d2ff7b44d04a0a669d10c3758ef962950186331c
Mahesh <mahesh@mahesh.ws>
2014-07-25 21:32:01 +0530

    Added Local Train Fragment

---
+++ mobile/src/main/java/ws/mahesh/travelmumbai/LocalFragment.java
@@ -0,0 +1,29 @@
+package ws.mahesh.travelmumbai;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mahesh on 7/25/2014.
+ */
+public class LocalFragment extends Fragment {
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.local_main_fragment, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
