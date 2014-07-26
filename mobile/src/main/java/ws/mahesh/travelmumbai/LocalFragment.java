a449b7c16c37565ff703dc415179f14723e936f1
Mahesh <mahesh@mahesh.ws>
2014-07-26 11:49:58 +0530

    Add Mega Block & Rail Map to Local

--- mobile/src/main/java/ws/mahesh/travelmumbai/LocalFragment.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/LocalFragment.java
@@ -1,16 +1,24 @@
 package ws.mahesh.travelmumbai;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import ws.mahesh.travelmumbai.misc.MegaBlockInfoActivity;
+import ws.mahesh.travelmumbai.misc.WebViewActivity;
 
 /**
  * Created by Mahesh on 7/25/2014.
  */
 public class LocalFragment extends Fragment {
+
+    Button MegaBlock,RailMap;
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -25,5 +33,25 @@ public class LocalFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Travel Mumbai - Local");
+        MegaBlock= (Button) getActivity().findViewById(R.id.buttonMegaBlockInfo);
+        RailMap= (Button) getActivity().findViewById(R.id.buttonRailMap);
+
+        MegaBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), MegaBlockInfoActivity.class));
+            }
+        });
+
+        RailMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), WebViewActivity.class);
+                i.putExtra("asset",true);
+                i.putExtra("location","railmap/railmap.html");
+                startActivity(i);
+            }
+        });
     }
 }
