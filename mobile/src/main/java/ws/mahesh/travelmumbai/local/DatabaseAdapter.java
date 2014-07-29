7cc5da1595ed33b2aa971952e52730e7e75206a7
Mahesh <mahesh@mahesh.ws>
2014-07-29 22:49:08 +0530

    Remove logs & add getTimeTableAllTrains method

--- mobile/src/main/java/ws/mahesh/travelmumbai/local/DatabaseAdapter.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/local/DatabaseAdapter.java
@@ -19,7 +19,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -64,15 +63,11 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
     private static int sr3;
     private static int sr4;
     private static String start;
-    private int poscount=0;
     private static String dest;
     private static String stname;
     private final Context myContext;
-    private SQLiteDatabase myDataBase;
-
     DateFormat f1 = new SimpleDateFormat("H:mm");
     DateFormat f2 = new SimpleDateFormat("h:mm a");
-
     static {
         DB_NAME = "timetable.sqlite";
         DB_NAME_PATH = "local/timetable.sqlite";
@@ -103,10 +98,12 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
         sqlitess = "null";
         sqliteds = "null";
         destinationtimetablequery = "null";
-        fullstationcode="null";
+        fullstationcode = "null";
         seltrain = "null";
         stname = "Starts";
     }
+    private int poscount = 0;
+    private SQLiteDatabase myDataBase;
 
     public DatabaseAdapter(Context paramContext) {
         super(paramContext, DB_NAME, null, DB_VERSION);
@@ -118,7 +115,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 
         try {
             DB_PATH = this.myContext.getDatabasePath(DB_NAME).getPath();
-            Log.i(getClass().getSimpleName(), "Checking DB existence at " + DB_PATH);
+            //    Log.i(getClass().getSimpleName(), "Checking DB existence at " + DB_PATH);
             SQLiteDatabase localSQLiteDatabase2 = SQLiteDatabase.openDatabase(DB_PATH, null, 1);
             localSQLiteDatabase1 = localSQLiteDatabase2;
         } catch (SQLiteException localSQLiteException) {
@@ -134,7 +131,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 
     private void copyDataBase()
             throws IOException {
-        Log.i(getClass().getSimpleName(), "Inside copyDatabase");
+        //  Log.i(getClass().getSimpleName(), "Inside copyDatabase");
         InputStream localInputStream = this.myContext.getAssets().open(DB_NAME_PATH);
         FileOutputStream localFileOutputStream = new FileOutputStream(DB_PATH);
         byte[] arrayOfByte = new byte[1024];
@@ -144,8 +141,8 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
                 localFileOutputStream.flush();
                 localFileOutputStream.close();
                 localInputStream.close();
-                Log.i(getClass().getSimpleName(), "Copy Database completed");
-                Log.i(getClass().getSimpleName(), "DB copied at " + DB_PATH);
+                //   Log.i(getClass().getSimpleName(), "Copy Database completed");
+                //    Log.i(getClass().getSimpleName(), "DB copied at " + DB_PATH);
                 return;
             }
             localFileOutputStream.write(arrayOfByte, 0, i);
@@ -154,12 +151,12 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 
     public void close() {
         try {
-            Log.i(getClass().getSimpleName(), "Inside close, DB being closed");
+            //      Log.i(getClass().getSimpleName(), "Inside close, DB being closed");
             if (this.myDataBase != null) {
                 this.myDataBase.close();
             }
             super.close();
-            Log.i(getClass().getSimpleName(), "Database Closed");
+            //      Log.i(getClass().getSimpleName(), "Database Closed");
             return;
         } finally {
         }
@@ -174,11 +171,11 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
             getWritableDatabase();
             return;
         }
-        Log.i(getClass().getSimpleName(), "DB Does not Exist, to be created");
-        Log.i(getClass().getSimpleName(), "Get Writable Database being called to create empty DB file");
+        //  Log.i(getClass().getSimpleName(), "DB Does not Exist, to be created");
+        //  Log.i(getClass().getSimpleName(), "Get Writable Database being called to create empty DB file");
         getWritableDatabase();
         try {
-            Log.i(getClass().getSimpleName(), "Copy Database being called from createDB");
+            //    Log.i(getClass().getSimpleName(), "Copy Database being called from createDB");
             copyDataBase();
             return;
         } catch (IOException localIOException) {
@@ -272,9 +269,118 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
                         DB_TABLE_TRAINS = DB_TABLE_HRDOWNTRAINS;
                     }
                 }
+                Base.time_in_minutes_max = 120 + Base.time_in_minutes;
+                this.myDataBase.execSQL("CREATE TEMPORARY TABLE sourcetimetable (_id INTEGER,trainkey TEXT,stkey TEXT,time TEXT,timemin INTEGER)");
+                sqlitess = "INSERT INTO sourcetimetable SELECT _id, trainkey, stkey, time, timemin FROM " + DB_TABLE_TIMETABLE + " WHERE stkey = " + "\"" + Base.Sourcevalcode + "\"" + " AND CAST(timemin AS INTEGER) >= " + Base.time_in_minutes + " AND CAST(timemin AS INTEGER) < " + Base.time_in_minutes_max + " ORDER BY CAST(timemin AS INTEGER)";
+                this.myDataBase.execSQL(sqlitess);
+                if (Base.time_in_minutes_max > 1439) {
+                    Base.time_in_minutes_max = -1439 + Base.time_in_minutes_max;
+                    sqlitess = "INSERT INTO sourcetimetable SELECT _id, trainkey, stkey, time, timemin FROM " + DB_TABLE_TIMETABLE + " WHERE stkey = " + "\"" + Base.Sourcevalcode + "\"" + " AND CAST(timemin AS INTEGER) < " + Base.time_in_minutes_max + " ORDER BY CAST(timemin AS INTEGER)";
+                    this.myDataBase.execSQL(sqlitess);
+                }
+                //  Log.i(getClass().getSimpleName(), "Source selection executed");
+                this.myDataBase.execSQL("CREATE TEMPORARY TABLE destinationtimetable (_id INTEGER,trainkey TEXT,stkey TEXT,time TEXT,timemin INTEGER)");
+                sqliteds = "INSERT INTO destinationtimetable SELECT _id, trainkey, stkey, time, timemin FROM " + DB_TABLE_TIMETABLE + " WHERE stkey = " + "\"" + Base.Destinationvalcode + "\"";
+                this.myDataBase.execSQL(sqliteds);
+                // Log.i(getClass().getSimpleName(), "Destination selection executed");
+                return;
+
+            } catch (SQLiteException localSQLiteException) {
+                Log.e(getClass().getSimpleName(), "Failed in SQLite Queries");
+                return;
+            }
+        }
+    }
+
+    public void createTempTimetableTableAllTrains() {
+        for (; ; ) {
+            try {
+                if (Base.trainLine.equals("CR")) {
+                    DB_TABLE_STATIONS = DB_TABLE_CRSTATIONS;
+                }
+                if (Base.trainLine.equals("WR")) {
+                    DB_TABLE_STATIONS = DB_TABLE_WRSTATIONS;
+                }
+                if (Base.trainLine.equals("HR")) {
+                    DB_TABLE_STATIONS = DB_TABLE_HRSTATIONS;
+                }
+                Cursor localCursor1 = this.myDataBase.rawQuery("SELECT * FROM " + DB_TABLE_STATIONS + " WHERE name = " + "\"" + Base.Sourcevaltxt + "\"" + " LIMIT 1", null);
+                if (localCursor1 != null) {
+                    if (localCursor1.moveToFirst()) {
+                        Base.Sourcevalcode = localCursor1.getString(localCursor1.getColumnIndex("code"));
+                        sr1 = localCursor1.getInt(localCursor1.getColumnIndex("r1"));
+                        sr2 = localCursor1.getInt(localCursor1.getColumnIndex("r2"));
+                        sr3 = localCursor1.getInt(localCursor1.getColumnIndex("r3"));
+                        sr4 = localCursor1.getInt(localCursor1.getColumnIndex("r4"));
+                    }
+                    localCursor1.close();
+                }
+                Cursor localCursor2 = this.myDataBase.rawQuery("SELECT * FROM " + DB_TABLE_STATIONS + " WHERE name = " + "\"" + Base.Destinationvaltxt + "\"" + " LIMIT 1", null);
+                if (localCursor2 != null) {
+                    if (localCursor2.moveToFirst()) {
+                        Base.Destinationvalcode = localCursor2.getString(localCursor2.getColumnIndex("code"));
+                        dr1 = localCursor2.getInt(localCursor2.getColumnIndex("r1"));
+                        dr2 = localCursor2.getInt(localCursor2.getColumnIndex("r2"));
+                        dr3 = localCursor2.getInt(localCursor2.getColumnIndex("r3"));
+                        dr4 = localCursor2.getInt(localCursor2.getColumnIndex("r4"));
+                    }
+                    localCursor2.close();
+                }
+                Base.updown = "blank";
+                if ((sr1 != 0) && (dr1 != 0)) {
+                    if (sr1 > dr1) {
+                        Base.updown = "U";
+                    } else
+                        Base.updown = "D";
+                } else if ((sr2 != 0) && (dr2 != 0)) {
+                    if (sr2 <= dr2) {
+                        Base.updown = "D";
+                    } else
+                        Base.updown = "U";
+                } else if ((sr3 != 0) && (dr3 != 0)) {
+                    if (sr3 <= dr3) {
+                        Base.updown = "D";
+                    } else
+                        Base.updown = "U";
+                } else if ((sr4 != 0) && (dr4 != 0)) {
+                    if (sr4 <= dr4) {
+                        Base.updown = "D";
+                    } else
+                        Base.updown = "U";
+                }
+                if (Base.trainLine.equals("CR")) {
+                    if (Base.updown.equals("U")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_CRUPTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_CRUPTRAINS;
+                    }
+                    if (Base.updown.equals("D")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_CRDOWNTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_CRDOWNTRAINS;
+                    }
+                }
+                if (Base.trainLine.equals("WR")) {
+                    if (Base.updown.equals("U")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_WRUPTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_WRUPTRAINS;
+                    }
+                    if (Base.updown.equals("D")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_WRDOWNTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_WRDOWNTRAINS;
+                    }
+                }
+                if (Base.trainLine.equals("HR")) {
+                    if (Base.updown.equals("U")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_HRUPTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_HRUPTRAINS;
+                    }
+                    if (Base.updown.equals("D")) {
+                        DB_TABLE_TIMETABLE = DB_TABLE_HRDOWNTIMETABLE;
+                        DB_TABLE_TRAINS = DB_TABLE_HRDOWNTRAINS;
+                    }
+                }
                 this.myDataBase.execSQL("CREATE TEMPORARY TABLE sourcetimetable (_id INTEGER,trainkey TEXT,stkey TEXT,time TEXT,timemin INTEGER)");
 
-                sqlitess = "INSERT INTO sourcetimetable SELECT _id, trainkey, stkey, time, timemin FROM " + DB_TABLE_TIMETABLE + " WHERE stkey = " + "\"" + Base.Sourcevalcode + "\""+ " ORDER BY CAST(timemin AS INTEGER)";
+                sqlitess = "INSERT INTO sourcetimetable SELECT _id, trainkey, stkey, time, timemin FROM " + DB_TABLE_TIMETABLE + " WHERE stkey = " + "\"" + Base.Sourcevalcode + "\"" + " ORDER BY CAST(timemin AS INTEGER)";
                 this.myDataBase.execSQL(sqlitess);
 
                 Log.i(getClass().getSimpleName(), "Source selection executed");
@@ -302,6 +408,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
     public int getPosCount() {
         return poscount;
     }
+
     public String[] getAllStations() {
         if (Base.trainLine.equals("CR")) {
             DB_TABLE_STATIONS = DB_TABLE_CRSTATIONS;
@@ -371,7 +478,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
     }
 
     public List<LocalItem> getTimeTable() {
-        poscount=0;
+        poscount = 0;
         if (Base.trainLine.equals("CR")) {
             DB_TABLE_STATIONS = DB_TABLE_CRSTATIONS;
         }
@@ -385,10 +492,10 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
         Cursor localCursor1 = this.myDataBase.rawQuery("SELECT * FROM sourcetimetable ", null);
         if (localCursor1 != null) {
             if (!localCursor1.moveToFirst()) {
-                Log.i(getClass().getSimpleName(), "No First Source record");
-                   return null;
+                //  Log.i(getClass().getSimpleName(), "No First Source record");
+                return null;
             }
-            Log.i(getClass().getSimpleName(), "First Source record called");
+            // Log.i(getClass().getSimpleName(), "First Source record called");
             do {
                 String str1 = localCursor1.getString(localCursor1.getColumnIndex("trainkey"));
                 seltrain = "SELECT * FROM " + DB_TABLE_TRAINS + " WHERE CAST (trainkey AS TEXT) = " + "\"" + str1 + "\"" + " LIMIT 1";
@@ -402,16 +509,16 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
                 if (localCursor2 != null) {
                     localCursor2.close();
                 }
-                fullstationcode = "SELECT * FROM "+DB_TABLE_STATIONS+" WHERE code = \"" + start + "\"";
+                fullstationcode = "SELECT * FROM " + DB_TABLE_STATIONS + " WHERE code = \"" + start + "\"";
                 Cursor localCursorsrc = this.myDataBase.rawQuery(fullstationcode, null);
                 if (localCursorsrc.moveToFirst()) {
-                    start=localCursorsrc.getString(localCursorsrc.getColumnIndex("name"));
+                    start = localCursorsrc.getString(localCursorsrc.getColumnIndex("name"));
                 }
                 localCursorsrc.close();
-                fullstationcode = "SELECT * FROM "+DB_TABLE_STATIONS+" WHERE code = \"" + dest + "\"";
+                fullstationcode = "SELECT * FROM " + DB_TABLE_STATIONS + " WHERE code = \"" + dest + "\"";
                 Cursor localCursordest = this.myDataBase.rawQuery(fullstationcode, null);
                 if (localCursordest.moveToFirst()) {
-                    dest=localCursordest.getString(localCursordest.getColumnIndex("name"));
+                    dest = localCursordest.getString(localCursordest.getColumnIndex("name"));
                 }
                 localCursordest.close();
                 destinationtimetablequery = "SELECT * FROM destinationtimetable WHERE trainkey = \"" + str1 + "\"";
@@ -420,16 +527,16 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
                     if (localCursor3.moveToFirst()) {
                         String str2 = localCursor1.getString(localCursor1.getColumnIndex("time"));
                         String str3 = localCursor3.getString(localCursor3.getColumnIndex("time"));
-                        fullstationcode = "SELECT * FROM "+DB_TABLE_STATIONS+" WHERE code = \"" + start + "\"";
+                        fullstationcode = "SELECT * FROM " + DB_TABLE_STATIONS + " WHERE code = \"" + start + "\"";
                         try {
                             localArrayList.add(new LocalItem(str1, f2.format(f1.parse(str2)), f2.format(f1.parse(str3)), start, dest, cars, speed));
-                            if(localCursor1.getInt(localCursor1.getColumnIndex("timemin"))<Base.time_in_minutes)
+                            if (localCursor1.getInt(localCursor1.getColumnIndex("timemin")) < Base.time_in_minutes)
                                 poscount++;
                         } catch (ParseException e) {
-                            localArrayList.add(new LocalItem(str1, str2,str3, start, dest, cars, speed));
+                            localArrayList.add(new LocalItem(str1, str2, str3, start, dest, cars, speed));
                         }
                         if (localCursor3.moveToNext()) {
-                            Log.i(getClass().getSimpleName(), "No First Source record");
+                            //Log.i(getClass().getSimpleName(), "No First Source record");
                         }
                     }
                     localCursor3.close();
@@ -492,10 +599,10 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
     }
 
     public void onUpgrade(SQLiteDatabase paramSQLiteDatabase, int paramInt1, int paramInt2) {
-        Log.i(getClass().getSimpleName(), "Inside OnUpgrade");
+        //Log.i(getClass().getSimpleName(), "Inside OnUpgrade");
         try {
-            Log.i(DatabaseAdapter.class.getName(), "Upgrading database from version " + paramInt1 + " to " + paramInt2);
-            Log.i(getClass().getSimpleName(), "Copy Database Method being called from onUpgrade");
+            //Log.i(DatabaseAdapter.class.getName(), "Upgrading database from version " + paramInt1 + " to " + paramInt2);
+            //Log.i(getClass().getSimpleName(), "Copy Database Method being called from onUpgrade");
             copyDataBase();
             return;
         } catch (IOException localIOException) {
@@ -507,11 +614,11 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
         if (!checkDataBase()) {
             createDataBase();
         }
-        Log.i(getClass().getSimpleName(), "Inside openDatabse DB being opened");
+        //Log.i(getClass().getSimpleName(), "Inside openDatabse DB being opened");
         DB_PATH = this.myContext.getDatabasePath(DB_NAME).getPath();
         String str = DB_PATH;
         this.myDataBase = SQLiteDatabase.openDatabase(str, null, 0);
-        Log.i(getClass().getSimpleName(), "DB opened at " + str);
+        //Log.i(getClass().getSimpleName(), "DB opened at " + str);
     }
 
 
