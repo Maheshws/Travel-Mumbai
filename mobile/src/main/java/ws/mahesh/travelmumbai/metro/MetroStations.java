f0df09798587947b2c4750a0485df48e3d3a5d4e
Mahesh <mahesh@mahesh.ws>
2014-07-29 13:49:05 +0530

    Added Metro Station Locations

--- mobile/src/main/java/ws/mahesh/travelmumbai/metro/MetroStations.java
+++ mobile/src/main/java/ws/mahesh/travelmumbai/metro/MetroStations.java
@@ -6,4 +6,6 @@ package ws.mahesh.travelmumbai.metro;
 public interface MetroStations {
     public static final int COUNT = 12;
     public static final String STATIONS[] = {"Versova", "D. N. Nagar", "Azad Nagar", "Andheri", "WEH", "Chakala", "Airport Road", "Marol Naka", "Saki Naka", "Asalpha", "Jagruti Nagar", "Ghatkopar"};
+    public static final Double LOCATIONS[][]={{19.130256,72.821371},{19.128505,72.830353},{19.126943, 72.837655},{19.120488,72.848700},{19.115789,72.856385},{19.112098, 72.867641},{19.110204, 72.874168},{19.108217, 72.879382},{19.103526, 72.887965},{19.096393, 72.894799},{19.092595, 72.901838},{19.086846, 72.908061}};
 }
+
