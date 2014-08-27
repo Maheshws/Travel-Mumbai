Maheshws/Travel-Mumbai

commit 391b4c5d9c95de72c151954994c4aad6d4092b2a

Fix Location bug
cont. location tracking bug

mobile/src/main/java/ws/mahesh/travelmumbai/LocalFragment.java
@@ -24,7 +24,8 @@ import ws.mahesh.travelmumbai.utils.CustomLocationListener;
 public class LocalFragment extends Fragment {
 
     Button MegaBlock,RailMap,WR,CR,HR;
-
+    LocationManager locationManager=null;
+    LocationListener locationListener;
 
     @Override
     public void onAttach(Activity activity) {
@@ -47,9 +48,9 @@ public class LocalFragment extends Fragment {
         CR= (Button) getActivity().findViewById(R.id.buttonCR);
         HR= (Button) getActivity().findViewById(R.id.buttonHR);
 
-        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-        LocationListener locationListener = new CustomLocationListener();
+        locationListener = new CustomLocationListener();
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
@@ -104,6 +105,12 @@ public class LocalFragment extends Fragment {
                 startActivity(i);
             }
         });
+    }
+    @Override
+    public void onPause(){
+        locationManager.removeUpdates(locationListener);
+        locationManager = null;
+        super.onPause();
     }
 
 }

mobile/src/main/java/ws/mahesh/travelmumbai/MetroFragment.java
@@ -43,6 +43,9 @@ public class MetroFragment extends Fragment {
     MetroFareBase metro=new MetroFareBase();
     private List<MetroListItem> metroItem=new ArrayList<MetroListItem>();
 
+    LocationManager locationManager=null;
+    LocationListener locationListener;
+
     public MetroFragment() {
         super();
     }
@@ -65,9 +68,9 @@ public class MetroFragment extends Fragment {
 
         final StationFinder sf=new StationFinder();
 
-        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-        LocationListener locationListener = new CustomLocationListener();
+        locationListener = new CustomLocationListener();
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
@@ -160,7 +163,11 @@ public class MetroFragment extends Fragment {
         return str;
     }
 
-
+    public void onPause(){
+        locationManager.removeUpdates(locationListener);
+        locationManager = null;
+        super.onPause();
+    }
 
 
 }

mobile/src/main/java/ws/mahesh/travelmumbai/MonorailFragment.java
@@ -42,6 +42,9 @@ public class MonorailFragment extends Fragment {
     ImageButton getLoc;
     MonoFareBase mono=new MonoFareBase();
 
+    LocationManager locationManager=null;
+    LocationListener locationListener;
+
     private List<MonoListItem> monoItem=new ArrayList<MonoListItem>();
 
     public MonorailFragment() {
@@ -64,9 +67,9 @@ public class MonorailFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Travel Mumbai - Monorail");
 
-        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-        LocationListener locationListener = new CustomLocationListener();
+        locationListener = new CustomLocationListener();
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
@@ -157,6 +160,13 @@ public class MonorailFragment extends Fragment {
             e.printStackTrace();
         }
         return str;
+    }
+
+    @Override
+    public void onPause(){
+        locationManager.removeUpdates(locationListener);
+        locationManager = null;
+        super.onPause();
     }
 
 

mobile/src/main/java/ws/mahesh/travelmumbai/TravelMumbai.java
@@ -109,7 +109,6 @@ public class TravelMumbai extends Application {
     private void getLocation() {
         int i = 0;
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new CustomLocationListener();
         do {
             if (i == 10)
                 break;
@@ -117,13 +116,11 @@ public class TravelMumbai extends Application {
             i++;
         } while (Base.lastKnownLocation == null);
         if (Base.lastKnownLocation != null) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
             Base.lastKnownLat = Base.lastKnownLocation.getLatitude();
             Base.lastKnownLon = Base.lastKnownLocation.getLongitude();
         }
         Base.lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         if (Base.lastKnownLocation != null) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, locationListener);
             Base.lastKnownLat = Base.lastKnownLocation.getLatitude();
             Base.lastKnownLon = Base.lastKnownLocation.getLongitude();
         }

commit 1a52a85e041fad96e8ad1642431b3413df2da5e7

Fix Location bugs
caused in 391b4c5d9c95de72c151954994c4aad6d4092b2a

mobile/src/main/java/ws/mahesh/travelmumbai/LocalFragment.java
@@ -7,6 +7,7 @@ import android.location.LocationManager;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,12 +48,6 @@ public class LocalFragment extends Fragment {
         WR= (Button) getActivity().findViewById(R.id.buttonWR);
         CR= (Button) getActivity().findViewById(R.id.buttonCR);
         HR= (Button) getActivity().findViewById(R.id.buttonHR);
-
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        locationListener = new CustomLocationListener();
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
         WR.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,9 +103,19 @@ public class LocalFragment extends Fragment {
     }
     @Override
     public void onPause(){
-        locationManager.removeUpdates(locationListener);
-        locationManager = null;
+        if(locationManager!=null) {
+            locationManager.removeUpdates(locationListener);
+            locationManager = null;
+        }
         super.onPause();
     }
-
+    @Override
+    public void onResume() {
+        if(locationManager==null) {
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new CustomLocationListener();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, locationListener);
+        }
+        super.onResume();
+    }
 }

mobile/src/main/java/ws/mahesh/travelmumbai/MetroFragment.java
@@ -68,12 +68,6 @@ public class MetroFragment extends Fragment {
 
         final StationFinder sf=new StationFinder();
 
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        locationListener = new CustomLocationListener();
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-
         source = (Spinner) getActivity().findViewById(R.id.spinnerSource);
 
         moreInfo = (Button) getActivity().findViewById(R.id.buttonmoreInfo);
@@ -163,11 +157,21 @@ public class MetroFragment extends Fragment {
         return str;
     }
 
+    @Override
     public void onPause(){
-        locationManager.removeUpdates(locationListener);
-        locationManager = null;
+        if(locationManager!=null) {
+            locationManager.removeUpdates(locationListener);
+            locationManager = null;
+        }
         super.onPause();
     }
-
-
+    @Override
+    public void onResume() {
+        if(locationManager==null) {
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new CustomLocationListener();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, locationListener);
+        }
+        super.onResume();
+    }
 }

mobile/src/main/java/ws/mahesh/travelmumbai/MonorailFragment.java
@@ -67,12 +67,6 @@ public class MonorailFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Travel Mumbai - Monorail");
 
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        locationListener = new CustomLocationListener();
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-
         source = (Spinner) getActivity().findViewById(R.id.spinnerSource);
 
         moreInfo = (Button) getActivity().findViewById(R.id.buttonmoreInfo);
@@ -121,7 +115,6 @@ public class MonorailFragment extends Fragment {
         });
         source.setSelection(sf.getNearbyMonoStation(Base.lastKnownLat,Base.lastKnownLon)+1);
     }
-
     private void setValues() {
         int current=source.getSelectedItemPosition();
         if(current>0)
@@ -164,11 +157,20 @@ public class MonorailFragment extends Fragment {
 
     @Override
     public void onPause(){
-        locationManager.removeUpdates(locationListener);
-        locationManager = null;
+        if(locationManager!=null) {
+            locationManager.removeUpdates(locationListener);
+            locationManager = null;
+        }
         super.onPause();
     }
-
-
+    @Override
+    public void onResume() {
+        if(locationManager==null) {
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new CustomLocationListener();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, locationListener);
+        }
+        super.onResume();
+    }
 
 }

mobile/src/main/java/ws/mahesh/travelmumbai/TravelMumbai.java
@@ -125,7 +125,5 @@ public class TravelMumbai extends Application {
             Base.lastKnownLon = Base.lastKnownLocation.getLongitude();
         }
 
-
-
     }
 }